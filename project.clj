24b838a482fe990d9202f54d3b56e9061e8dcb0f
Levi Roth <levimroth@gmail.com>
2017-02-26 21:36:02 -0500

    Change license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cs50-clojure "0.1.0-SNAPSHOT"
   :description "Clojure implementation of some CS50 problem sets"
   :url "http://example.com/FIXME"
-  :license {:name "CC BY-NC-SA 3.0"
-            :url "https://creativecommons.org/licenses/by-nc-sa/3.0/"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot cs50-clojure.core
   :target-path "target/%s"
